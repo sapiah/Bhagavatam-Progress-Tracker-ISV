016190322b96dbdbed43509d9ddf28c2b4f120a5
Sachin Apiah <sachinappiah20@gmail.com>
2023-07-23 01:22:22 +0400

    Added and positioned feet

---
+++ script.js
@@ -0,0 +1,12 @@
+const img = document.querySelectorAll("img"); 
+img[0].src = "images/govardhanTracker.jpg";
+img[0].className="center-fit"
+
+img[1].src = "images/Unstepped-feet.png"
+img[1].className="feet-1"
+
+img[2].src = "images/Unstepped-feet.png"
+img[2].className="feet-2"
+
+// img[3].src = "images/Unstepped-feet.png"
+// img[3].className="feet-1"
