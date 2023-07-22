0bdfe381b85548171be801c3972b0c79031cb850
Sachin Apiah <sachinappiah20@gmail.com>
2023-07-23 02:22:06 +0400

    Placement of 20 steps through the path

--- script.js
+++ script.js
@@ -16,3 +16,48 @@ img[4].className="feet-4"
 
 img[5].src = "images/Unstepped-feet.png"
 img[5].className="feet-5"
+
+img[6].src = "images/Unstepped-feet.png"
+img[6].className="feet-6"
+
+img[7].src = "images/Unstepped-feet.png"
+img[7].className="feet-7"
+
+img[8].src = "images/Unstepped-feet.png"
+img[8].className="feet-8"
+
+img[9].src = "images/Unstepped-feet.png"
+img[9].className="feet-9"
+
+img[10].src = "images/Unstepped-feet.png"
+img[10].className="feet-10"
+
+img[11].src = "images/Unstepped-feet.png"
+img[11].className="feet-11"
+
+img[12].src = "images/Unstepped-feet.png"
+img[12].className="feet-12"
+
+img[13].src = "images/Unstepped-feet.png"
+img[13].className="feet-13"
+
+img[14].src = "images/Unstepped-feet.png"
+img[14].className="feet-14"
+
+img[15].src = "images/Unstepped-feet.png"
+img[15].className="feet-15"
+
+img[16].src = "images/Unstepped-feet.png"
+img[16].className="feet-16"
+
+img[17].src = "images/Unstepped-feet.png"
+img[17].className="feet-17"
+
+img[18].src = "images/Unstepped-feet.png"
+img[18].className="feet-18"
+
+img[19].src = "images/Unstepped-feet.png"
+img[19].className="feet-19"
+
+img[20].src = "images/Unstepped-feet.png"
+img[20].className="feet-20"
